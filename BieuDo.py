7efa5ed2a16ab626f3ba3a72c1db1e1661cd702e
Akashi169 <162897309+Akashi169@users.noreply.github.com>
2024-11-17 10:33:56 +0700

    add feature create column and update file BieuDo.py

--- BieuDo.py
+++ BieuDo.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-df_details = pd.read_csv("E:\\phu\\Details.csv", sep=",", header = 0)
-df_orders = pd.read_csv("E:\\phu\\Orders.csv", sep=",", header = 0)
+df_details = pd.read_csv("Details.csv", sep=",", header = 0)
+df_orders = pd.read_csv("Orders.csv", sep=",", header = 0)
 df_merged = pd.merge(df_details, df_orders, on = 'Order ID', how = 'left')
 
 df_sorted = df_merged.sort_values(by="Order ID")
@@ -19,45 +19,44 @@ print("\nInformation general del DataFrame:")
 print(df_online_sales.info())
 
 
-
-# Tổng hợp dữ liệu
+# # Tổng hợp dữ liệu
 total_sales = df_online_sales['Amount'].sum()
 total_profit = df_online_sales['Profit'].sum()
 total_customers = df_online_sales['CustomerName'].nunique()
 total_oders = len(df_online_sales)
 
 
-# Tổng doanh số (Total Sales)
+# # Tổng doanh số (Total Sales)
 fig, ax = plt.subplots(2,2, figsize = (12,8))
 ax[0,0].bar('Total Sales' , total_sales, color = 'blue')
 ax[0,0].text('Total Sales', total_sales, str(total_sales), ha = 'center', va = 'bottom',fontsize = 10 ) 
 ax[0,0].set_ylabel('Amount')
 ax[0,0].set_title('Total Sales')
 
-# Tổng lợi nhuận (Total Profitability)
+# # Tổng lợi nhuận (Total Profitability)
 ax[0,1].bar('Total profitability', total_profit, color = 'green')
 ax[0,1].text('Total profitability', total_profit,str(total_profit), ha = 'center', va = 'bottom', fontsize = 10)
 ax[0,1].set_ylabel('Amount')
 ax[0,1].set_title('Total profitability')
 
 
-#Số khách hàng duy nhất (Total Customers)
+# #Số khách hàng duy nhất (Total Customers)
 ax[1,0].bar('Total customers', total_customers, color='orange')
 ax[1,0].text('Total customers', total_customers, str(total_customers), ha='center', va='bottom')
 ax[1,0].set_ylabel('Amount')  
 ax[1,0].set_title('Total customers')
 
-#Số đơn hàng hoàn thành (Completed Sales)
+# #Số đơn hàng hoàn thành (Completed Sales)
 ax[1,1].bar('Completed Sales', total_oders, color = 'red' )
 ax[1,1].text('Completed Sales', total_oders, str(total_oders), ha = 'center', va = 'bottom', fontsize = 10)
 ax[1,1].set_ylabel('Amount')
 ax[1,1].set_title('Completed Sales')
 
-# Điều chỉnh khoảng cách giữa các biểu đồ và hiển thị chúng.
+# # Điều chỉnh khoảng cách giữa các biểu đồ và hiển thị chúng.
 plt.tight_layout()
 plt.show()
 
-# Đếm số lượng giao dịch theo thành phố và phương thức thanh toán, sau đó tìm phương thức phổ biến nhất ở mỗi thành phố.
+# # Đếm số lượng giao dịch theo thành phố và phương thức thanh toán, sau đó tìm phương thức phổ biến nhất ở mỗi thành phố.
 city_payment_counts = df_online_sales.groupby(['City', 'PaymentMode']).size().reset_index(name = 'Count')
 city_most_common_payment = city_payment_counts.loc[city_payment_counts.groupby('City')['Count'].idxmax()]
 print(city_most_common_payment)
@@ -76,7 +75,7 @@ plt.title('Number of Purchases by Category and Months')
 plt.xlabel('Months')
 plt.ylabel('Number of Purchases ')
 plt.xticks(range(1, 13), ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
-plt.legend(title='Categoría')
+plt.legend(title='Category')
 plt.grid(True)
 plt.show()
 
